0d391919374dc8ef8fab044f33677303d055a56f
takumidev86 <takumidevelopment86@gmail.com>
2020-11-04 00:26:43 +0900

    [Fix] prperties.py

--- properties.py
+++ properties.py
@@ -10,10 +10,9 @@ database = 'sotorrent18_09'
 
 
 # Data dump paths data_dump_original_path にSoTorrentをダウンロードをしたディレクトリを設定する
-data_dump_original_path = "/home/takumi-k/e/data_dump_original_path/"
+data_dump_original_path = "/mnt/e/data_dump_original_path/"
 # data_dump_final_path を最終データ ダンプを保存するディレクトリに設定する
-data_dump_final_path = "/home/takumi-k/e/data_dump_final_path/"
+data_dump_final_path = "/mnt/e/data_dump_final_path/"
 # 方法論の全てのデータを保存させるパスに設定する
-data_path = "/home/takumi-k/e/data_path/"
-sequence_extractor_path = "home/takumi-k/e/sequence_extractor_path/SequenceExtractor-0.4.jar"
-``
+data_path = "/mnt/e/data_path/"
+sequence_extractor_path = "/mnt/e/sequence_extractor_path/SequenceExtractor-0.4.jar"
