a81f39b7b0c8c9b061537a902b63584759b81fbd
takumidev86 <takumidevelopment86@gmail.com>
2020-11-04 00:22:37 +0900

    [Add] pro

--- properties.py
+++ properties.py
@@ -5,12 +5,15 @@ password = ''
 # password = 'password'
 host = '127.0.0.1'
 port = '3306'
-database = 'SOTorrent_db'
-# database = 'sotorrent18_09'
+# database = 'SOTorrent_db'
+database = 'sotorrent18_09'
 
 
-# Data dump paths
-data_dump_original_path = "/path/to/original/dump/"
-data_dump_final_path = "/path/to/java/dump/"
-data_path = "/path/to/data/"
-sequence_extractor_path = "/path/to/SequenceExtractor-0.4.jar"
+# Data dump paths data_dump_original_path にSoTorrentをダウンロードをしたディレクトリを設定する
+data_dump_original_path = "/home/takumi-k/e/data_dump_original_path/"
+# data_dump_final_path を最終データ ダンプを保存するディレクトリに設定する
+data_dump_final_path = "/home/takumi-k/e/data_dump_final_path/"
+# 方法論の全てのデータを保存させるパスに設定する
+data_path = "/home/takumi-k/e/data_path/"
+sequence_extractor_path = "home/takumi-k/e/sequence_extractor_path/SequenceExtractor-0.4.jar"
+``
