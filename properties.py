97e7fbefad59108a3010e2a4df9d685586c8b145
takumidev86 <takumidevelopment86@gmail.com>
2020-11-06 12:47:44 +0900

    [Add] properties

--- properties.py
+++ properties.py
@@ -1,7 +1,7 @@
 # DB settings
-user = 'root'
+user = 'takumi'
 # user = 'sotorrent'
-password = ''
+password = '0987xcdh'
 # password = 'password'
 host = '127.0.0.1'
 port = '3306'
@@ -15,4 +15,4 @@ data_dump_original_path = "/mnt/e/data_dump_original_path/"
 data_dump_final_path = "/mnt/e/data_dump_final_path/"
 # 方法論の全てのデータを保存させるパスに設定する
 data_path = "/mnt/e/data_path/"
-sequence_extractor_path = "/mnt/e/sequence_extractor_path/SequenceExtractor-0.4.jar"
+sequence_extractor_path = "/mnt/e/sequence_extractor_path/sequenceextractor.py"
